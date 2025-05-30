4ce27561a02f0cc45027a87a26d816c000f21f86
Patricia <patricia.gthb@gmail.com>
2025-05-30 01:04:58 -0300

    proyecto pre-entrega

---
+++ main.py
@@ -0,0 +1,134 @@
+lista_productos = [["nombre", "categoría", "precio"], ["a","B",10],["taza","vajilla", 10], ["plato", "vajilla", 30],["taza", "desayuno", 10]]
+
+
+#lista_productos =[]
+
+
+
+while True:
+
+  print("Menu de Opciones:")
+  print("(1) . Agregar productos")
+  print("(2) . Mostrar productos")
+  print("(3) . Buscar productos")
+  print("(4) . Eliminar productos")
+  print("(5) . Salir")
+
+  opcion = input("Seleccione una opcion: ").strip()
+  if opcion.isdigit():
+    opcion = int(opcion)
+  else:
+    print("Entrada inválida, por favor ingrese un número") #falta mje si el numero es mayor a 5
+    continue
+
+  match opcion:
+    case 1: #Agrega productos
+      while True:
+        while True:
+          nombre_producto = input("Ingrese el nombre del producto: ").strip().lower()
+          if nombre_producto == "":
+            print("El nombre del producto no puede estar vacío.")
+          else:
+            break
+
+        while True:
+          categoria_producto = input("Ingrese la categoría del producto: ").strip().lower()
+          if categoria_producto == "":
+            print("La categoría del producto no puede estar vacía.")
+          else:
+            break
+
+        while True:
+          precio_producto = input("Ingrese el precio del producto: ").strip()
+
+          if precio_producto == "":
+            print("El precio del producto no puede estar vacío.")
+            continue
+          if precio_producto.isdigit():
+            precio_producto = int(precio_producto)
+          else:
+            print("El precio del producto deber ser un número entero(sin decimales) y no puede ser negativo.")
+            continue
+
+          break
+
+
+
+        lista_productos.append([nombre_producto, categoria_producto, precio_producto])
+        print(f"Se ha agregado el producto '{nombre_producto.capitalize()}' en la categoria '{categoria_producto.capitalize()}' con el precio $ {precio_producto}")
+
+        #prueba salida
+        while True:
+          respuesta = input("Desea agregar otro producto? s / n : ").strip().lower()
+
+          if respuesta == "n":
+           break
+          elif respuesta == "s":
+            break
+          else:
+            print("Respuesta inválida. Por favor ingrese s / n : ")
+        if respuesta == "n":
+            break
+
+
+    case 2:
+      if len(lista_productos) == 0:
+        print("No hay productos en la lista.")
+      else:
+        lista_productos.sort()
+        for indice in range(len(lista_productos)):
+          print(f"{indice+1}. \t{lista_productos[indice][0]}\t{lista_productos[indice][1]}\t{lista_productos[indice][2]}")
+
+
+    case 3:
+      producto_buscado = input("Ingrese el nombre del producto que desea buscar: ").strip().lower()
+      productos_encontrados = []
+
+      for producto in lista_productos:
+        if producto[0] == producto_buscado:
+          productos_encontrados.append(producto)
+
+
+      if productos_encontrados:
+        print(f"Se han encontrado los siguientes productos: {productos_encontrados}\n")
+
+      else:
+        print(f"El producto buscado no se encuentra en la lista\n")
+
+
+
+    #Eliminar productos
+    case 4:
+      if len(lista_productos) == 0:
+        print("No hay productos en la lista")
+
+      else:
+        for indice in range(len(lista_productos)):
+          print(f"{indice+1}. \t{lista_productos[indice][0]}\t{lista_productos[indice][1]}\t{lista_productos[indice][2]}")
+
+        producto_a_eliminar = input("Ingrese el número de orden en el que se encuentra el producto a eliminar: ").strip()
+        if producto_a_eliminar.isdigit():
+          int(producto_a_eliminar)
+        else:
+          print("Entrada inválida.")
+
+        #falta validacion numero
+        #respuesta si se ingresa un numero fuera de rango , evitar el error
+        #corregir error cuando se ingresa una palabra
+
+        producto_eliminado = lista_productos.pop(int(producto_a_eliminar)-1)
+        print(f"Se ha eliminado el producto {producto_eliminado}")
+
+
+    case 5:
+      print("Hasta pronto!")
+      break
+
+    case _n:
+      print("La opcion ingresada no es válida (desde el nro 1 al nr 5)")
+
+
+
+
+
+
